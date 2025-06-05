375aa4f97e91d799008beccb0f0d4568bcb98e99
4-0-4 <146018190+nyeinkokoaung404@users.noreply.github.com>
2025-06-05 14:16:28 +0630

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <div className=" min-h-screen flex gap-5 p-5 pt-20 items-center w-full flex-col sm:pt-28">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Xery Youtube Downloader
+          404 Youtube Downloader
         </h1>
 
         <Form fetchData={fetchData} />
